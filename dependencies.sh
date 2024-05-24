12fbeb8f2a7f309e2ca5687539be864c150c927d
Angello20 <68455791+Angello20@users.noreply.github.com>
2024-05-23 23:45:42 -0600

    Add HuffmanEncodeUTF8 files

--- dependencies.sh
+++ dependencies.sh
@@ -7,10 +7,6 @@ echo "Actualizando paquetes del sistema"
 sudo dnf update -y
 
 echo "Compilando archivos"
-gcc parallel_huffman_encode_utf.c -o parallel_encode
-gcc parallel_huffman_decode_utf.c -o parallel_decode
 gcc huffman_encode_utf.c -o encode
 gcc huffman_decode_utf.c -o decode
-gcc concurrent_huffman_encode_utf.c -o concurrent_encode -lpthread
-gcc concurrent_huffman_decode_utf.c -o concurrent_decode -lpthread
 echo "Proceso finalizado"
